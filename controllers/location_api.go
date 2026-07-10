f140719388081ca5f029b5eae09c62be10cdc5fc
Mojammel Haque Bhuiyan <mhbhuiyan10023@gmail.com>
2026-07-10 16:23:41 +0600

    feat: add location service GetLocation function

--- controllers/location_api.go
+++ controllers/location_api.go
@@ -2,20 +2,26 @@ package controllers
 
 import (
 	"refine-portal/services"
-
 	"github.com/beego/beego/v2/server/web"
+
+	"strings"
 )
 
 type LocationAPIController struct {
 	web.Controller
 }
 
-func (c *LocationAPIController) GetLocation() {
+func (c *LocationAPIController) Get() {
 	keyword := c.GetString("keyword")
+
+	if strings.TrimSpace(keyword) == "" {
+		c.CustomAbort(400, "Keyword is required")
+		return
+	}
 
 	location, err := services.GetLocation(keyword)
 	if err != nil {
-		c.CustomAbort(500, err.Error())
+		c.CustomAbort(500, "Internal Server Error")
 		return
 	}
 
--- services/location_service.go
+++ services/location_service.go
@@ -1,5 +1,75 @@
 package services
 
-import "refine-portal/models"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"refine-portal/models"
+	"strings"
+	"time"
 
-func GetLocation(keyword string) (*models.LocationResponse, error)
+	"github.com/beego/beego/v2/server/web"
+)
+
+const (
+	locationAPIPath = "/api/location/v1"
+)
+
+func GetLocation(keyword string) (*models.LocationResponse, error) {
+	// Get the base url
+	baseURL, err := web.AppConfig.String("base_url")
+	if err != nil {
+		return nil, fmt.Errorf("failed to read 'base_url' from configuration: %w", err)
+	}
+
+	if strings.TrimSpace(baseURL) == "" {
+		return nil, fmt.Errorf("configuraion 'base_url' is empty")
+	}
+	
+	// Build URL
+	parsedURL, err := url.Parse(baseURL) // Validates and converts the raw string into a structured *url.URL object.
+	if err != nil {
+		return nil, fmt.Errorf("parse base url: %w", err)
+	}
+
+	parsedURL.Path = locationAPIPath // append the endpoint path 
+
+	query := parsedURL.Query() // Extract existing query parameters into a url.Values map
+	query.Set("keyword", keyword)
+	query.Set("isLocationEntity", "true")
+	parsedURL.RawQuery = query.Encode() // Marshals the map back into a raw string
+
+	// Client HTTP request
+	req, err := http.NewRequest(http.MethodGet, parsedURL.String(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("create request: %w", err)
+	}
+
+	req.Header.Set("Accept", "application/json")
+
+	// Send request 
+	client := &http.Client{
+		Timeout: 10*time.Second,
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("send request: %w", err)
+	}
+	defer res.Body.Close()
+
+	// Check response status
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d", res.StatusCode)
+	}
+
+	// Decode response 
+	var location models.LocationResponse
+
+	if err := json.NewDecoder(res.Body).Decode(&location); err != nil {
+		return nil, fmt.Errorf("decode response: %w", err)
+	}
+
+	return &location, nil
+}
